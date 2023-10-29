8b06b86eb73b2184ff71d1bc811866e321d90b6e
Roberto <roberto.v.arebalo@gmail.com>
2023-10-29 12:53:28 -0300

    Ejercicio 6 terminado

--- js/ejercicio6.js
+++ js/ejercicio6.js
@@ -1,1 +1,12 @@
-//Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+//Escribe un programa que pida dos números y escriba en la pantalla cual es el mayorr
+const primerNumero = parseInt(prompt("Mostrare cual es el numero mayor de dos que vas a ingresar; ingresa el primer numero:"));
+const segundoNumero = parseInt(prompt("Ingresa el segundo numero:"));
+let numeroMayor = 0 ;
+
+if (primerNumero>segundoNumero) {
+    numeroMayor = primerNumero;
+} else {
+    numeroMayor =segundoNumero;
+}
+
+alert("El numero mayor es: " + numeroMayor);
