49a74d55209db015ab20fe15940ae48d662cef2a
Roberto <roberto.v.arebalo@gmail.com>
2023-10-29 18:37:41 -0300

    Ejercicio 8 terminado

--- js/ejercicio8.js
+++ js/ejercicio8.js
@@ -1,1 +1,12 @@
 // Escribe un programa que pida un número y diga si es divisible por 2
+
+const tuNumero = parseInt(prompt("Te voy a indicar si un numero es divisible por 2, ingresa el numero que deseas consultar: "));
+let esDivisible = "no";
+
+if ((tuNumero % 2)> 0) {
+    esDivisible = "no es ";
+} else {
+    esDivisible = "es ";
+}
+
+alert("tu numero " + esDivisible + "dibisible por 2");
