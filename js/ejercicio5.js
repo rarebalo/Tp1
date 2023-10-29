1b23ce1ff30d90e0c8b446356eef2fcbcc27be56
Roberto <roberto.v.arebalo@gmail.com>
2023-10-29 12:37:30 -0300

    Se agrega el ejercicio 5 completo

---
+++ js/ejercicio5.js
@@ -0,0 +1,3 @@
+const primerNumero = parseInt(prompt("Vamos a hacer una suma entre dos numeros, escribe el primer numero"));
+const segundoNumero = parseInt(prompt("Ahora escribe el sengo numero"));
+alert("La suma de " + primerNumero + " + " + segundoNumero + " es igual a: " + (primerNumero+segundoNumero));
