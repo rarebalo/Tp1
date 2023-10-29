209387f74920b9a6707d87c6fe4bd46ad6271978
Roberto <roberto.v.arebalo@gmail.com>
2023-10-29 13:35:00 -0300

    Ejercicio 7 terminado

--- js/ejercicio7.js
+++ js/ejercicio7.js
@@ -1,1 +1,16 @@
 //Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres
+const primerNumero = parseInt(prompt("Te dire cual es el mayor numero de 3 que vas a ingresar; ingresa el primer numero: "));
+const segundoNumero = parseInt(prompt("Ingresa el segundo numero: "));
+const terceroNumero = parseInt(prompt("Ingresa el tercer numero: "));
+let numeroMayor = 0;
+
+if (primerNumero>segundoNumero && primerNumero>terceroNumero) {
+    numeroMayor = primerNumero;
+} else if(segundoNumero>terceroNumero) {
+    numeroMayor = segundoNumero;
+}
+else{
+    numeroMayor = terceroNumero;
+}
+
+alert("El numero mayor es: " + numeroMayor);
