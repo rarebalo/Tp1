3cc18a470ef12c7505724a2ec735d30d313fad41
Roberto <roberto.v.arebalo@gmail.com>
2023-10-30 16:45:14 -0300

    Falta capturar cada vocal y mostrar al final

--- js/ejercicio9.js
+++ js/ejercicio9.js
@@ -1,14 +1,19 @@
 /*Escribe un programa que pida una frase y escriba las vocales que aparecen
 Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)*/
 
-const laFrase = prompt("Escribe una frase y te dire las bocales que contiene");
-let postCadena=[];
+const laFrase = prompt("Escribe una frase y te dire las vocales que contiene");
+let vocales = [];
+
+alert(laFrase);
 
 for (let index = 0; index < laFrase.length; index++) {
-    if (laFrase.charAt(index) == (a || e || i || o || u)) {
-         
-        alert(laFrase.charAt(index)); 
-    }    
+   
+    if (laFrase.charAt(index) == "a" && "e" && "i" && "o" && "u") {
+        //alert(laFrase.charAt(index));
+        vocales.push(laFrase.charAt(index));
+    }        
+    alert(index);
 }
 
-alert("esto: " + postCadena);
+
+
