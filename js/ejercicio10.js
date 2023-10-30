f6a7932b9fe23588547686e23b9923f82c156779
Roberto <roberto.v.arebalo@gmail.com>
2023-10-30 20:38:20 -0300

    Agrego ejercicio 10 terminado

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,1 +1,20 @@
 //10.- Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+
+const numeroUsuario = parseInt(
+  prompt("Ingrese un numero para comprobar si es divisible por 2, 3, 5 o 7: ")
+);
+const divisores = [2, 3, 5, 7];
+let capturaDivisor = 0;
+
+for (let index = 0; index < divisores.length; index++) {
+  if (numeroUsuario % parseInt(divisores[index]) === 0) {
+    capturaDivisor = divisores[index];
+    break;
+  }
+}
+
+if (capturaDivisor > 0) {
+  alert(`El ${numeroUsuario} es divisible en ${capturaDivisor}`);
+} else {
+  alert(`El ${numeroUsuario} no es divisble por 2, 3, 5 ni 7 :(`);
+}
