523bbb3f98206852f902fc35f9db0d19f6bd6d83
Roberto <roberto.v.arebalo@gmail.com>
2023-10-30 08:17:59 -0300

    El ejercicio esta mal planteado por el momento no funciona

---
+++ js/ejercicio9.js
@@ -0,0 +1,14 @@
+/*Escribe un programa que pida una frase y escriba las vocales que aparecen
+Nota: Tener en cuenta la función length y substring o charAt (developer mozilla)*/
+
+const laFrase = prompt("Escribe una frase y te dire las bocales que contiene");
+let postCadena=[];
+
+for (let index = 0; index < laFrase.length; index++) {
+    if (laFrase.charAt(index) == (a || e || i || o || u)) {
+         
+        alert(laFrase.charAt(index)); 
+    }    
+}
+
+alert("esto: " + postCadena);
